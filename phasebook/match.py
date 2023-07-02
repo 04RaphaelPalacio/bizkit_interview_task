d736f808be8efc37a35c2d59ccd0fd092dcdec29
Raphael Ledesma <raphaelledesma04@outlook.com>
2023-07-02 17:36:11 +0800

    new changes

--- phasebook/match.py
+++ phasebook/match.py
@@ -23,5 +23,23 @@ def is_match(fave_numbers_1, fave_numbers_2):
     for number in fave_numbers_2:
         if number not in fave_numbers_1:
             return False
+    for number in fave_numbers_2:
+        if number in fave_numbers_2 == fave_numbers_1:
+            return True
+        
+        
 
-    return True
+#Pseudo_code 
+#(Match endpoint for Matching Algorithm is not found: http://127.0.0.1:5000/match/%7Bmatch_id%7D and {match id} to test takes too long to respond: 127.0.0.1:5000/match/3 )
+
+'''
+list1 = [1,2,3,4,5,6,7]    
+list2 = [1,2,3,4,5,6,8]
+    
+for number in list2:
+    if number not in list1:
+        print('false')
+for number in list2:
+    if number in list2 == list1:
+        print('true')
+'''
